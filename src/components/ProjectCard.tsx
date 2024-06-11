b30c73ebb5c2fd649969b1c29b2e55bd0fa7ac98
KalilovM <mn.kalilov@gmail.com>
2024-06-11 22:29:16 +0200

    Creating header Fixing Project card component

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -2,54 +2,25 @@ import reactLogo from '../assets/react.svg'
 
 const ProjectCard = () => {
   return (
-    <div className="rounded-2xl bg-white flex flex-row">
-      <div><img src={reactLogo} alt="#"/></div>
-      <div className="flex flex-col items-start justify-center text-start">
-        <div>
-          <span>Company name</span>
-          <span>10 June</span>
+    // TODO: Implement rating for the company
+    <div className="rounded-sm bg-gray-700 px-6 py-6 text-base">
+      <div className="min-h-[125px] flex flex-row">
+        <a className="w-12 h-12 overflow-hidden">
+          <img src={reactLogo} alt="#" className="w-full h-full object-cover"/>
+        </a>
+        <div className="flex flex-col flex-1 text-white">
+          <div className="justify-between flex">
+            <a href="#">Apphud</a>
+            <time dateTime="2024-06-11T20:16:16+03:00">11 june</time>
+          </div>
+          <div className="overflow-hidden break-words font-bold text-xl leading-none">
+            <a href="#">Project team collector</a>
+          </div>
+          <div className="mt-1">
+            <span className="text-base after:content-[',_'] last:after:content-['']">Moscow</span>
+            <span className="text-base after:content-[',_'] last:after:content-['']">Online</span>
+          </div>
         </div>
-        <h2>Project name</h2>
-        <span className="">
-          <span>
-          <span>County</span>
-            <span> • </span>
-          </span>
-          <span>
-          <span>online</span>
-            <span> • </span>
-          </span>
-        </span>
-        <span className="">
-          <span>
-            <span>Rust</span>
-            <span> • </span>
-          </span>
-          <span>
-          <span>Senior</span>
-            <span> • </span>
-          </span>
-          <span>
-          <span>Product manager</span>
-            <span> • </span>
-          </span>
-          <span>
-          <span>Scrum master</span>
-            <span> • </span>
-          </span>
-          <span>
-          <span>React</span>
-            <span> • </span>
-          </span>
-          <span>
-          <span>Frontend</span>
-            <span> • </span>
-          </span>
-          <span>
-            <span>OOP</span>
-            <span> • </span>
-          </span>
-        </span>
       </div>
     </div>
   );
